3a67f63757d38179bebc0268c88df39d6e780932
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-03-24 10:57:47 -0300

    Initial Commit

---
+++ WhatIsThat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatIsThat
+//
+//  Created by Cem Akkaya on 24/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatIsThat/WhatIsThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsThatApp.swift
+//  WhatIsThat
+//
+//  Created by Cem Akkaya on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
